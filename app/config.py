14e0688e2e0d8c378bc35c32edadc7fd8c600e0d
Akshay Patel <akshyrcks@gmail.com>
2026-08-05 15:57:46 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
-CAPTAIN_BASE_PRICE = int(os.getenv("CAPTAIN_BASE_PRICE", 400000))
+CAPTAIN_BASE_PRICE = int(os.getenv("CAPTAIN_BASE_PRICE", 300000))
 PLAYER_BASE_PRICE = int(os.getenv("PLAYER_BASE_PRICE", 50000))
-MIN_SQUAD_SIZE = int(os.getenv("MIN_SQUAD_SIZE", 13))
+MIN_SQUAD_SIZE = int(os.getenv("MIN_SQUAD_SIZE", 12))
 MAX_PURSE = int(os.getenv("MAX_PURSE", 2500000))
 TIMER_SECONDS = int(os.getenv("TIMER_SECONDS", 90))
 REVEAL_SECONDS = int(os.getenv("REVEAL_SECONDS", 12))
@@ -14,10 +14,10 @@ SKILL_CATEGORIES = ["Batting", "Bowling", "All-Rounder [Batting]", "All-Rounder 
 
 # (ceiling, increment) - applies while current_bid < ceiling
 INCREMENT_SLABS = [
-    (50000, 5000),
-    (100000, 10000),
-    (200000, 20000),
-    (300000, 25000),
+    (100000, 5000),
+    (200000, 10000),
+    (300000, 20000),
+    (400000, 25000),
     (float("inf"), 50000),
 ]
 
